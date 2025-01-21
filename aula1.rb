5e0b90bb9468dc6b7f36706152e9df14d50b2768
NicolyKethylin <kethylinswan@outlook.com>
2025-01-21 20:00:32 -0300

    Primeiros erros

---
+++ aula1.rb
@@ -0,0 +1,17 @@
+puts "Meu primeiro programa em Ruby!"
+
+#1) saida de dados na tela --> aparecer um comando na tela
+
+#usuário digita seu nome
+print "Digite seu nome: "
+#2) permita que o usuário preencha o que foi pedido
+# gets.chump - metodo de entrada 
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+#3) saida final
+puts "Oi. Tudo bem, #{nome} #{sobrenome}?  "
+
+
---
+++ aulasoma.rb
@@ -0,0 +1,12 @@
+#soma entre os numero
+print "Digite um número inteiro: "
+
+num1 = gets.chomp.to_i # to = para e i = inteiro
+
+print "Digite outro numero: "
+
+num2 =gets.chomp.to_i
+
+soma = num1 + num2
+
+print "A soma dos dois numeros é #{soma}"
